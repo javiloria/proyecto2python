e6b84d5893e12034c4fb86c67503e56e3f804075
herick1 <herick200@gmail.com>
2020-01-06 16:51:53 -0400

    haciendo reporte uno compleeto

--- gestorAppTG/gestorAppTG/views/reporte.py
+++ gestorAppTG/gestorAppTG/views/reporte.py
@@ -8,15 +8,22 @@ from django.contrib.auth.decorators import login_required
 from ..models import *
 from django.utils.decorators import method_decorator
 from ..decorador import *
-
+from django import forms
+from django.db.models import Q
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+import xlwt
+import datetime
 
 @method_decorator([login_required, gestor_permisos], name='dispatch')
 class IndexReporte1View(generic.ListView):
     template_name = 'reporte/reporte1.html'
     context_object_name = 'propuesta_list_Reporte1'
     def get_queryset(self):
-        return Propuesta.objects.filter(estatus__nombre="Aprobada").order_by('estudiante_1__cedula')
-        #primer reporte filtar porque no sean aprobadas y por la cedula 
+        rows1 = Propuesta.objects.exclude( estatus__nombre="Aprobada" ).filter(estudiante_1__isnull=False).select_related('estatus','escuela','estudiante_1','titulo','termin').values_list('estudiante_1__cedula','estudiante_1__primer_apellido','estudiante_1__segundo_apellido','estudiante_1__primer_nombre','estudiante_1__segundo_nombre','termin__id','titulo') 
+        rows = Propuesta.objects.exclude( estatus__nombre="Aprobada" ).filter(estudiante_2__isnull=False).select_related('estatus','escuela','estudiante_2','titulo','termin').values_list('estudiante_2__cedula','estudiante_2__primer_apellido','estudiante_2__segundo_apellido','estudiante_2__primer_nombre','estudiante_2__segundo_nombre','termin__id','titulo').union(rows1) 
+        return rows
+        #reporte excluyendo a los que esten aprobados y poniendo 
 
 
 @method_decorator([login_required, gestor_permisos], name='dispatch')
